16195cfbd6f2c4a3fc57783860a7285d38e7f4d7
vladosik24 <kuriliak.official@gmail.com>
2025-12-18 19:51:21 +0200

    Remove Telegram score transmission comment
    Removed comment about sending score to Telegram.

--- game.js
+++ game.js
@@ -4,7 +4,6 @@ function addScore() {
   score++;
   document.getElementById("score").innerText = score;
 
-  // Передаємо очки в Telegram
   if (window.TelegramGameProxy) {
     TelegramGameProxy.setScore(score);
   }
