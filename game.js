89a2b69828dc47310378eb861f8ea6ddeca2114c
vladosik24 <kuriliak.official@gmail.com>
2025-12-18 20:13:51 +0200

    Update game.js

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ function addScore() {
   score++;
   document.getElementById("score").innerText = score;
 
+  // Telegram API
   if (window.TelegramGameProxy) {
     TelegramGameProxy.setScore(score);
   }
